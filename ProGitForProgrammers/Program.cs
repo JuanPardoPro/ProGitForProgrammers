4d781b045b2cdf9baf877d8ea966aba3ef6d2e9d
Juan David Pardo <jhondeivid12@hotmail.com>
2022-01-07 15:31:28 -0500

    Cambio agregado por usuario 1 en rama main

--- ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/Program.cs
@@ -11,6 +11,7 @@ namespace ProGitForProgrammers
             Console.WriteLine("I just added this in the command line repo");
             Console.WriteLine("This line added to show the staging area");
             Console.WriteLine("Codigo agregado por el programador 2, la idea es generar un merge conflict, se resuelve con kdiff3 uniendo remoto con local");
+            Console.WriteLine("Cambio realizado por programmer 1 en la rama main");
         }
     }
 }
