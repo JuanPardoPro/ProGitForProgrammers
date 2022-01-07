d01ce2ccba92bd356a16f1c1d296d415461c08f0
Programmer1 <jhondeivid12@hotmail.com>
2022-01-07 14:05:42 -0500

    Primer commit programdor 1

--- ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/Program.cs
@@ -10,6 +10,7 @@ namespace ProGitForProgrammers
             Console.WriteLine("I just added this in Visual Studio");
             Console.WriteLine("I just added this in the command line repo");
             Console.WriteLine("This line added to show the staging area");
+            Console.WriteLine("Codigo agregado por el programador 1");
         }
     }
 }
